c387454bb8071d8996cb3e13e3558da75afd08f5
coolmanbtr <Amirjonbtr@gmail.com>
2025-05-15 09:43:10 -0400

    Update PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,1 +1,12 @@
-public int hkufi
+//Part A
+public int walkDogs(int hours){
+   //Find out from DogWalk Company how many dogs at hour
+   int numDogs = company.numAvaibleDogs(hour);
+   if(maxDogs > numDogs){
+       company.updateDogs(hour,numDogs);
+       return numDogs;
+   }
+   //if there are more/equal dogs than the walker can handle
+   company.updateDogs(hour,maxDogs);
+   return maxDogs;
+}
