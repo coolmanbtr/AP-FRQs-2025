71e0f7466d14a3bf5c576b1c1ba9532e5a40cfb3
coolmanbtr <Amirjonbtr@gmail.com>
2025-05-15 09:44:16 -0400

    Update PartB.java

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -1,1 +1,17 @@
+public int dogWalkShift(int startHour, int endHour){
+   int totalPay =  0;
+   for(int i = startHour;i <= endHour; i++){
+       int dogsWalked = walkDogs(hour);
+
+
+       int pay = dogsWalked * 5;//Base pay
+
+
+       if(dogsWalked == maxDogs || (hour >= 9 && hour <= 17)){
+           pay += 3;
+       }
+       totalPay += pay;
+   }
+   return totalPay;
+}
 
